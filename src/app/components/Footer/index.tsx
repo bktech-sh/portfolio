f640d70393198995a2cb00ba2b11ac026de13941
nugrahaa878 <nugrahaa878@gmail.com>
2024-06-17 22:37:58 +0700

    feat: finish footer

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import {
+  BackdropFooter1,
+  BackdropFooter2,
+  BackdropFooter3,
   ButtonContactUs,
+  Col1,
+  Col2,
   ContactInfo,
   FooterContainer,
   PageLink,
@@ -10,6 +15,8 @@ import bktechIcon from "@/app/assets/images/footer/bktech.png";
 import instagramIcon from "@/app/assets/images/footer/instagram.png";
 import twitterIcon from "@/app/assets/images/footer/twitter.png";
 import facebookIcon from "@/app/assets/images/footer/facebook.png";
+import backdropBottom from "@/app/assets/images/contact/backdrop-bottom.png";
+import backdropTop from "@/app/assets/images/contact/backdrop-top.png";
 
 const Footer = () => {
   return (
@@ -26,7 +33,28 @@ const Footer = () => {
 
         <ButtonContactUs>Contact Us</ButtonContactUs>
       </ContactInfo>
-      <PageLink></PageLink>
+
+      <PageLink>
+        <Col1>
+          <p>Work With Us</p>
+          <p>About Us</p>
+          <p>Our Work</p>
+        </Col1>
+        <Col2>
+          <p>FAQs</p>
+          <p>Report a Bug</p>
+        </Col2>
+      </PageLink>
+
+      <BackdropFooter1>
+        <Image src={backdropBottom} alt="backdrop-1" width={300} />
+      </BackdropFooter1>
+      <BackdropFooter2>
+        <Image src={backdropBottom} alt="backdrop-2" width={200} />
+      </BackdropFooter2>
+      <BackdropFooter3>
+        <Image src={backdropBottom} alt="backdrop-3" width={400} />
+      </BackdropFooter3>
     </FooterContainer>
   );
 };
--- src/app/components/Footer/styles.ts
+++ src/app/components/Footer/styles.ts
@@ -9,12 +9,15 @@ export const FooterContainer = styled.div`
     ${BktechColors.darkPurple}
   );
   color: white;
+  padding: 50px 100px;
+  justify-content: space-between;
+  position: relative;
+  overflow: hidden;
 `;
 
 export const ContactInfo = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 50px 100px;
   gap: 20px;
 `;
 
@@ -25,6 +28,8 @@ export const SocialMedia = styled.div`
 
 export const PageLink = styled.div`
   display: flex;
+  gap: 50px;
+  padding-right: 20%;
 `;
 
 export const ButtonContactUs = styled.button`
@@ -34,4 +39,36 @@ export const ButtonContactUs = styled.button`
   border-radius: 30px;
   color: white;
   font-size: 16px;
+  opacity: 100%;
+  z-index: 2;
+`;
+
+export const Col1 = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+export const Col2 = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+export const BackdropFooter1 = styled.div`
+  position: absolute;
+  left: 0;
+  bottom: -50%;
+`;
+
+export const BackdropFooter2 = styled.div`
+  position: absolute;
+  top: -30%;
+  left: 50%;
+`;
+
+export const BackdropFooter3 = styled.div`
+  position: absolute;
+  right: 0;
+  bottom: 0;
 `;
